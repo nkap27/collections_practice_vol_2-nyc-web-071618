0f106d53cb827390cfb8b3156729f0e365d57a6d
Natasha K <kapr.natasha@gmail.com>
2018-07-15 22:29:45 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -10,11 +10,11 @@ def begins_with_r(tools)
 end
 
 def contain_a(elements)
-  elements.each do |element|
-    letter_a = []
-    if element.include?("a")
+ letter_a = []
+ elements.each do |element|
+   if element.include?("a")
       letter_a << element
     end
-    return letter_a
   end
+  return letter_a
 end
